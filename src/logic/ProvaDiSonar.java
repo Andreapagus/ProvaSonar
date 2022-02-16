18fdd893ed87d8ab479d7febe04bf2b55b2f5eb0
Andreapagus <34506169+Andreapagus@users.noreply.github.com>
2022-02-16 18:55:17 +0100

    Added lines of code

---
+++ src/logic/ProvaDiSonar.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class ProvaDiSonar {
+	private static int ok = 1;
+	
+	public static void main(String[] args) {
+		System.out.println("Ciao mondo! "+String.valueOf(ok));
+		
+	}
+	
+}
